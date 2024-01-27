2818d670d82f841a04306369a04b0d13781743c4
Routin hugo <routin.hugo74@orange.fr>
2024-01-27 09:21:16 +0100

    fichiers originaux prof

---
+++ common.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+LENGTH = 4
+COLORS = ['R', 'V', 'B', 'J', 'N', 'M', 'O', 'G']
+# Notez que vos programmes doivent continuer à fonctionner si on change les valeurs par défaut ci-dessus
